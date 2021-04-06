d31e513f09ba7c6d7a7bc17365e32f59b72d7957
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-06 16:51:08 +0300

    Work with Head component

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,12 @@
-
+import Head from 'next/head'
 
 const about = () => {
   return (
     <div>
+      <Head>
+        <title>About</title>
+        <meta name='keywords' content='web dev' />
+      </Head>
       <h1>About</h1>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Mezidia</title>
+        <meta name='keywords' content='web dev' />
+      </Head>
       <h1>Welcome to Next</h1>
     </div>
   )
